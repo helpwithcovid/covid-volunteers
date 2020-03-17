493c5079c342d822eed38b13f21315044fa3f26d
Radu Spineanu <radu.spineanu@gmail.com>
2020-03-17 15:51:30 -0700

    hopefully fix the weird rails crash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true, with: :exception
+
   def ensure_admin
     redirect_to projects_path if !current_user || !current_user.is_admin?
   end
