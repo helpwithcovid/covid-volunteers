d3662cf4d2d9522a98ef1692dcb4c06dbfd135b4
Radu Spineanu <radu.spineanu@gmail.com>
2020-03-18 00:12:34 -0700

    show flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -52,4 +52,19 @@ module ApplicationHelper
   def logout_nav_link_class(variant = 'DESKTOP')
     nav_link_inactive_class(variant)
   end
+
+  def alert_container_class_for_flash_type type
+    base_class = 'border px-4 py-3 rounded relative'
+
+    Rails.logger.error type
+    if [ 'alert', 'error'].include?(type)
+      base_class += ' bg-red-100 border-red-400 text-red-700'
+    elsif type == 'notice'
+      base_class += ' bg-green-100 border-green-400 text-green-700'
+    else
+      base_class += ' bg-orange-100 border-orange-400 text-orange-700'
+    end
+
+    base_class
+  end
 end
