f286b32cb3cb818b57c68584b3d66c1a31ffc289
Radu Spineanu <radu.spineanu@gmail.com>
2020-03-17 19:55:07 -0700

    add an url in the email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
