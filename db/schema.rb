c4a7948f3783fa8ea4124a6e1e7289a213fa1052
OrlandoLee <zongshengli@ucla.edu>
2020-03-18 19:39:24 -0700

    ol - filter by skill

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,6 @@ ActiveRecord::Schema.define(version: 2020_03_18_215539) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "likes", force: :cascade do |t|
-    t.integer "project_id"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "offers", force: :cascade do |t|
     t.integer "user_id"
     t.string "name", default: "", null: false
@@ -36,9 +29,8 @@ ActiveRecord::Schema.define(version: 2020_03_18_215539) do
   create_table "projects", force: :cascade do |t|
     t.integer "user_id"
     t.string "name", default: "", null: false
-    t.string "participants", default: "", null: false
     t.string "description", default: "", null: false
-    t.string "goal", default: "", null: false
+    t.string "participants", default: "", null: false
     t.string "looking_for", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
