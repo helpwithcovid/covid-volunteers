fb5ae686d9c6881a70da19939b6dcee2204e9894
Jamie Wilkinson <jamie@jamiedubs.com>
2020-03-20 15:00:02 -0400

    Add name to project URL slugs
    Keep id prefix for compatibility with simple id lookups

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,6 +11,10 @@ class Project < ApplicationRecord
 
   pg_search_scope :search, against: %i(name description participants looking_for location highlight), using: { tsearch: { any_word: true } }
 
+  def to_param
+    [id, name.parameterize].join("-")
+  end
+
   def volunteer_emails
     self.volunteered_users.collect { |u| u.email }
   end
