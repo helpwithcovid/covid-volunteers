5efa4941682ea8b103c0b72931099bf29ae29545
OrlandoLee <zongshengli@ucla.edu>
2020-03-19 19:07:40 -0700

    ol - solve conflict

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,10 @@
 ALL_SKILLS = [ 'Software', 'Biology', 'Biotech', 'Medicine', 'Mechanics & Electronics', 'Funding', 'Content',  'Manufacturing', 'PM', 'Anything' ].freeze
-
+ALL_PROJECT_TYPES = [ 
+  'Track the outbreak', 
+  'Help out neighbours', 
+  'News and information', 
+  'Medical supplies', 
+  'Test the virus', 
+  'Other'
+  ].freeze
 VOLUNTEERS_REQUIRED_FOR_FUNDING = 50
