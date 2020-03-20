d446f4cc0110a6089581f139a3155cd330e2d9a0
Radu Spineanu <radu.spineanu@gmail.com>
2020-03-19 19:11:24 -0700

    add missing file

--- config/initializers/availability.rb
+++
@@ -1,2 +0,0 @@
-# need to be lower case so that availabilty are checked in checkbox
-ALL_AVAILABILITY = [ '1-2 hours a day', '2-4 hours a day', '4+ hours a day', 'Only on Weekends', 'Full Time' ].freeze
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,4 @@
 ALL_SKILLS = [ 'Software', 'Biology', 'Biotech', 'Medicine', 'Mechanics & Electronics', 'Funding', 'Content',  'Manufacturing', 'PM', 'Anything' ].freeze
+ALL_AVAILABILITY = [ '1-2 hours a day', '2-4 hours a day', '4+ hours a day', 'Only on Weekends', 'Full Time' ].freeze
 
 VOLUNTEERS_REQUIRED_FOR_FUNDING = 50
