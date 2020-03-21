39e915a7f6289d5b06d5b407a77486221b156b2e
Adrian Marin <adrian@adrianthedev.com>
2020-03-21 12:51:11 +0200

    feat: badges for project types on index page

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -4,4 +4,14 @@ module ProjectsHelper
 
     project_class = 'border-2 border-orange-300 bg-orange-100' if project.highlight
   end
+
+  def badges_for_project_types(project)
+    to_display = 3
+
+    if project.project_type_list.count > to_display
+      extra = project.project_type_list.dup.drop(to_display).count
+    end
+
+    render partial: 'badges_for_project_types', locals: {project_types: project.project_type_list.take(to_display), extra: extra}
+  end
 end
