af92e961db9dc84ae9741ed4d4aed8f979253633
Adrian Marin <adrian@adrianthedev.com>
2020-03-21 18:15:55 +0200

    fix: n+1 on projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,7 +16,7 @@ class ProjectsController < ApplicationController
     else
       grouped_projects = filtered_projects.left_joins(:volunteers).group(:id)
     end
-    @projects = grouped_projects.order('highlight DESC, COUNT(volunteers.id) DESC, created_at DESC').page(params[:page]).per(25)
+    @projects = grouped_projects.includes(:project_types).order('highlight DESC, COUNT(volunteers.id) DESC, created_at DESC').page(params[:page]).per(25)
 
     @index_from = (@projects.prev_page || 0) * @projects.current_per_page + 1
     @index_to = [@index_from + @projects.current_per_page - 1, @projects.total_count].min
