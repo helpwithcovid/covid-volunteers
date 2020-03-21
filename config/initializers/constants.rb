b222c411d6f12079869890f61b44619d2ede99c6
Adrian Marin <adrian@adrianthedev.com>
2020-03-21 11:46:40 +0200

    fix: project type filters

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,19 +1,19 @@
 ALL_SKILLS = [ 'Software', 'Biology', 'Biotech', 'Medicine', 'Mechanics & Electronics', 'Funding', 'Content',  'Manufacturing', 'PM', 'Anything' ].freeze
 ALL_AVAILABILITY = [ '1-2 hours a day', '2-4 hours a day', '4+ hours a day', 'Only on Weekends', 'Full Time' ].freeze
 
-ALL_PROJECT_TYPES = [ 
-  'Track the outbreak', 
+ALL_PROJECT_TYPES = [
+  'Track the outbreak',
   'Reduce spread',
   'Scale testing',
   'Medical facilities',
   'Medical equipments',
-  'Treatment R&D', 
-  'Help out communities', 
-  'Map volunteers to needs',
-  'News and information',
+  'Treatment R&D',
   'E-Learning',
   'Job placement',
   'Mental health',
+  'Help out communities',
+  'Map volunteers to needs',
+  'News and information',
   'Social giving',
   'Other'
   ].freeze
