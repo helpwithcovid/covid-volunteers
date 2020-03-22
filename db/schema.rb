206bdd153eec8c4b2b41f8ea3572f6876494b117
Nagesh Bansal <bansalnagesh@gmail.com>
2020-03-22 17:49:32 +0530

    Asking if users want to be paired with relevant projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_210730) do
+ActiveRecord::Schema.define(version: 2020_03_22_121011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 2020_03_21_210730) do
     t.boolean "visibility", default: false
     t.string "name", default: "", null: false
     t.string "level_of_availability"
+    t.boolean "pair_with_projects", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
