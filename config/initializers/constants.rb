8f22a2d255cff97ed606b66db3e91dde6fa47431
Alex Chen <alex.chen.ac@yale.edu>
2020-03-29 17:05:26 -0400

    Redo skills list

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,23 @@
-ALL_SKILLS = [ 'Biology', 'Biotech', 'Content', 'Design', 'Funding', 'Manufacturing', 'Medicine', 'Mechanics & Electronics', 'PM', 'Software', 'Volunteer vetting', 'Anything' ].freeze
+ALL_SKILLS = [ 
+  'Access to car', 
+  'Arts and crafts', 
+  'BHL (biohazard level) lab certificate', 
+  'Childcare', 
+  'Communications', 
+  'CPR certificate', 
+  'Culinary skills and access to kitchen', 
+  'Database architecture', 
+  'Driver\'s license (passenger)', 
+  'Driver\'s license (commercial)', 
+  'Door-to-door canvassing', 
+  'EPIC access', 
+  'Grant writing', 
+  'IRB experience', 
+  'Logistics and supply chain management',
+  'Phlebotomy', 
+  'Phone bank', 
+  'Statistical analysis'
+].freeze
 ALL_AVAILABILITY = [ '1-2 hours a day', '2-4 hours a day', '4+ hours a day', 'Only on Weekends', 'Full Time' ].freeze
 ALL_NUMBER_OF_VOLUNTEERS = [ '1-10', '10-50', '50-100', '100+' ]
 ALL_LEVELS_OF_URGENCY = [ 'Extremely urgent', 'Very urgent', 'Moderately urgent', 'Somewhat urgent', 'Not urgent' ]
