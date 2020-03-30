791d0531be2724309ba2a211010fd3b59103cbd3
Radu Spineanu <radu.spineanu@gmail.com>
2020-03-30 12:00:10 -0700

    revert session store change, causing more issues

--- config/initializers/session_store.rb
+++
@@ -1,2 +0,0 @@
-# Based on https://stackoverflow.com/questions/20875591/actioncontrollerinvalidauthenticitytoken-in-registrationscontrollercreate.
-Rails.application.config.session_store :cookie_store, expire_after: 14.days
