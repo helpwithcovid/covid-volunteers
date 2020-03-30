3996d63297907611ebcaed2312cabb3059e18421
Radu Spineanu <radu.spineanu@gmail.com>
2020-03-30 14:14:24 -0700

    send project information about how many projects a user volunteered for

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -2,6 +2,7 @@ class ProjectMailer < ApplicationMailer
   def new_volunteer
     @project = params[:project]
     @user = params[:user]
+    @user_volunteered_projects_count = @user.volunteers.count
 
     mail(to: @project.user.email, subject: "You got a new volunteer for #{@project.name}!")
   end
