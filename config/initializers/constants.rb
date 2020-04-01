d0552a00f55fb18d0be35747a2b8b452dec5fb4d
Nagesh Bansal <bansalnagesh@gmail.com>
2020-04-01 12:53:27 +0530

    Global and Resource announcements

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -28,3 +28,28 @@ ALL_PROJECT_STATUS = [
 
 VOLUNTEERS_REQUIRED_FOR_FUNDING = 50
 MAX_VOLUNTEERS_FOR_HIGHLIGHT_OFFER = 30
+
+RESOURCE_ANNOUNCEMENTS = [
+  {
+    title: 'HWC Project & Volunteer Playbook',
+    sub_title: 'A short guide created by our team to help you successfully manager your project and volunteers!',
+    cta_text: 'Download Playbook',
+    cta_url: "https://docs.google.com/document/d/1f_-g893NGawa5n4KyDw6zjrocO2VBS5b2siQIZ6csY0/view"
+  },
+  {
+    title: 'Webinar on How to Scale Volunteers',
+    sub_title: 'Glen Moriarty, founder of 7cups on how he manages 320k volunteers.',
+    cta_text: 'View Recording',
+    cta_url: "https://openai.zoom.us/rec/play/vZN-dbuq-m03TNKSsASDAfEtW466J6qs1XQWrvtYxRvmU3QDNVv0NLJGYbffjdiMo1gf68qHVnbe2mOi?continueMode=true"
+  }
+]
+
+GLOBAL_ANNOUNCEMENTS = [
+  {
+    title: 'Resources for your project',
+    sub_title: 'From webinars on How to Scale Volunteers, management playbook, to office hours, offers and more.',
+    cta_text: 'View Resources',
+    # TODO find a better way to pass this
+    cta_url: 'offers'
+  }
+]
