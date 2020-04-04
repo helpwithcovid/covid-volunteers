0002487947321f20a0323261f3d90d01a7558395
Sarim Abbas <sarim.stays@gmail.com>
2020-04-04 17:32:32 -0400

    add integration with mailchimp for newsletter consent
    1. added a new column in db for newsletter_consent (boolean)
    2. added a form control in the profile page to control the db value
    3. everytime the profile is saved, the newsletter_consent var is checked. If true, a request is made to Mailchimp via Gibbon, which subscribes the email
    4. when the user is first registered, they are opted in to the newsletter by default

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'gibbon'
 
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
@@ -63,4 +64,42 @@ class User < ApplicationRecord
   def active_for_authentication?
     super && !self.deactivated
   end
+
+  # this function uses Gibbon and Mailchimp API to subscribe/unsubscribe users
+  def subscribe_to_mailchimp(action = true)
+    gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
+    gibbon.timeout = 15
+    list_id = ENV['MAILCHIMP_LIST_ID']
+
+    response = gibbon.lists(list_id).members(Digest::MD5.hexdigest(self.email)).upsert(body: {
+        email_address: self.email, 
+        status: action ? "subscribed" : "unsubscribed", 
+    })
+  
+    response
+  end
+
+  # this function checks the newsletter_consent field in before_save
+  def check_newsletter_consent
+    if self.newsletter_consent
+        subscribe_to_mailchimp(true)
+    else 
+        subscribe_to_mailchimp(false)
+    end
+  end
+
+  # this function is used with before_create
+  def opt_into_newsletter_on_sign_up
+    self.newsletter_consent = true
+    subscribe_to_mailchimp(true)
+  end   
+
+  # before saving, we check if the user opted in or out, 
+  # if so they will be subscribed or unsubscribed
+  # TODO: prevent unnecessary requests to mailchimp by checking the previous state
+  before_save :check_newsletter_consent
+
+  # after sign up, the user will be opted into the newsletter by default
+  before_create :opt_into_newsletter_on_sign_up
+
 end
