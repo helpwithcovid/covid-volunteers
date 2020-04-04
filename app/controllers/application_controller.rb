d11112380139b41391418a27e50be8cb1cb7988b
Adrian Marin <adrian@adrianthedev.com>
2020-04-04 12:10:37 +0300

    refactor: bring back home controller code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true, with: :exception
+  before_action :set_project_groups
 
   def ensure_admin
     redirect_to projects_path if !current_user || !current_user.is_admin?
@@ -21,5 +22,43 @@ class ApplicationController < ActionController::Base
 
   def show_global_announcements
     @show_global_announcements = true
+  end
+
+  def set_project_groups
+    @project_groups = Rails.cache.fetch('project_groups', expires_in: 1.hour) do
+      groups = [
+        {
+          title: 'Prevention',
+          body: 'Inform, reduce, and control the spread of the outbreak',
+          project_skills: ['Reduce spread', 'Scale testing'],
+          icon: 'medical',
+          featured: true,
+          featured_projects: [],
+        },
+        {
+          title: 'Medical',
+          body: 'Support our overwhelmed health systems with medical innovation',
+          project_skills: ['Medical facilities', 'Medical equipments'],
+          icon: 'medical',
+          featured: true,
+          featured_projects: [],
+        },
+        {
+          title: 'Community',
+          body: 'Work together to help our stressed communities',
+          project_skills: ['Social giving', 'Help out communities'],
+          icon: 'medical',
+          featured: true,
+          featured_projects: [],
+        },
+      ]
+
+      groups.map do |group|
+        group[:featured_projects] = Project.tagged_with(group[:project_skills]).take 9
+        group[:projects_count] = Project.tagged_with(group[:project_skills]).count
+      end
+
+      groups
+    end
   end
 end
