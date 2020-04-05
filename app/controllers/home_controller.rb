4130232782c49b69b2f2aab607d70af78087d05f
Adrian Marin <adrian@adrianthedev.com>
2020-04-05 22:18:16 +0300

    refactor: increase cache invalidation to 1 day and get hightlighted projects

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,10 @@ class HomeController < ApplicationController
   before_action :hide_global_announcements
 
   def index
-    @project_count = Rails.cache.fetch('project_count', expires_in: 1.hour) { Project.count }.tap { |count| (count / 50).floor * 50 }
-    @volunteer_count = Rails.cache.fetch('volunteer_count', expires_in: 1.hour) { User.count }.tap { |count| (count / 100).floor * 50 }
-    @featured_projects = Rails.cache.fetch('featured_projects', expires_in: 1.hour) { Project.take 3 }
+    # Display the projects in increments of 50
+    @project_count = Rails.cache.fetch('project_count', expires_in: 1.day) { Project.count }.tap { |count| (count / 50).floor * 50 }
+    # Display the volunteers in increments of 50
+    @volunteer_count = Rails.cache.fetch('volunteer_count', expires_in: 1.day) { User.count }.tap { |count| (count / 100).floor * 50 }
+    @featured_projects = Rails.cache.fetch('featured_projects', expires_in: 1.day) { Project.where(highlight: true).order('RANDOM()').take 3 }
   end
 end
