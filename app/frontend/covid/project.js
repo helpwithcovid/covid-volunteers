27cbe49c6f0c86573d48e9def7b20ff760cbae0b
Radu Spineanu <radu.spineanu@gmail.com>
2020-04-05 19:49:32 -0700

    be consistent and show modal for both situations

--- app/frontend/covid/project.js
+++ app/frontend/covid/project.js
@@ -4,6 +4,10 @@ const Project = {
       $('#volunteer_with_skills').click(function(ev) {
         Project.volunteerWithSkills(this, ev);
       });
+
+      $('#volunteer_without_skills').click(function(ev) {
+        Project.volunteerWithoutSkills(this, ev);
+      });
     });
   },
 
@@ -18,7 +22,23 @@ const Project = {
 
     const callback = () => $.post(targetHref);
     Covid.showModal(headerHTML, bodyHTML, [ { type: 'cancel' }, { type: 'submit', text: 'Volunteer', callback } ], 'warning');
+
+    return false;
+  },
+
+  volunteerWithoutSkills(that, ev) {
     ev.preventDefault();
+    ev.stopPropagation();
+
+    const targetHref = $(that).attr('href');
+    const skillsRequired = $(that).attr('x-skills-required');
+
+    const headerHTML = "You're missing skills";
+    const bodyHTML = `It looks like the skills needed for this project do not match your skillset. \n\nIf you think this is incorrect, please update your profile with one of the following skills: <b>${skillsRequired}</b>.`;
+
+    const callback = () => window.location.href = targetHref;
+    Covid.showModal(headerHTML, bodyHTML, [ { type: 'cancel' }, { type: 'submit', text: 'Edit Profile', callback } ], 'warning');
+
     return false;
   }
 }
