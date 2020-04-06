1442e1f8f4f75445f776da0f730c0b903e89b171
Mark Santolucito <mark.santolucito@yale.edu>
2020-04-06 10:12:46 -0400

    make migration work on empty string

--- db/migrate/20200405125534_change_start_date_to_date.rb
+++ db/migrate/20200405125534_change_start_date_to_date.rb
@@ -1,7 +1,7 @@
 class ChangeStartDateToDate < ActiveRecord::Migration[6.0]
   def up
-    change_column :projects, :start_date, :date, using: 'start_date::DATE', default: nil
-    change_column :projects, :end_date, :date, using: 'start_date::DATE', default: nil
+    change_column :projects, :start_date, 'date USING CAST(case when start_date = \'\' then null else start_date end AS date)', default: nil
+    change_column :projects, :end_date, 'date USING CAST(case when end_date = \'\' then null else end_date end AS date)', default: nil
   end
 
   def down
