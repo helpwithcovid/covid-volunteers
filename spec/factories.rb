0feff3c65325797b8360e504d7e941515f20996e
Casey Pugh <156097+caseypugh@users.noreply.github.com>
2020-04-06 11:59:00 -0400

    fix spec to just check for presence
    fixes staging test builds

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,12 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :volunteer_group do
-    
   end
 
-
   factory :user do
-    name { "Bob Isok" }
-    sequence(:email) {|n|
+    name { 'Bob Isok' }
+    sequence(:email) { |n|
       # puts "HI"; puts n.inspect; "user#{n}@example.com"
       gen = "user_#{rand(1000)}@factory.com"
       while User.where(email: gen).exists?
@@ -14,26 +14,27 @@ FactoryBot.define do
       end
       gen
     }
-    password { "df823jls18fk350f" }
+    password { 'df823jls18fk350f' }
   end
 
   factory :user_complete_profile, parent: :user do
-    about { "About" }
-    profile_links { "Profile" }
-    location { "location" }
+    about { 'About' }
+    profile_links { 'Profile' }
+    location { 'location' }
   end
 
-	factory :project do
-    name { "My First Project" }
+  factory :project do
+    name { 'My First Project' }
+    description { 'My description' }
+    location { 'location' }
     status { ALL_PROJECT_STATUS.first }
   end
 
-	factory :volunteer do
-		# ...
-	end
+  factory :volunteer do
+    # ...
+  end
 
-	factory :offer do
-		# ...
-	end
-
+  factory :offer do
+    # ...
+  end
 end
