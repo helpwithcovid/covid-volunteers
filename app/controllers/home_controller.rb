3c6477d857cc5468a3917e26344b26a7396eff16
Adrian Marin <adrian@adrianthedev.com>
2020-04-07 18:41:57 +0300

    refactor: break rails.cache.fetch block

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,21 @@ class HomeController < ApplicationController
   before_action :hide_global_announcements
 
   def index
+    @project_count = Rails.cache.fetch('project_count', expires_in: 1.day) do
+      Project.count
+    end
     # Display the projects in increments of 50
-    @project_count = Rails.cache.fetch('project_count', expires_in: 1.day) { Project.count }.tap { |count| (count / 50).floor * 50 }
-    # Display the volunteers in increments of 50
-    @volunteer_count = Rails.cache.fetch('volunteer_count', expires_in: 1.day) { User.count }.tap { |count| (count / 100).floor * 50 }
-    @featured_projects = Rails.cache.fetch('featured_projects', expires_in: 1.day) { Project.where(highlight: true).order('RANDOM()').take 3 }
+    @project_count = (@project_count / 50).floor * 50
+
+    @volunteer_count = Rails.cache.fetch('volunteer_count', expires_in: 1.day) do
+      User.count
+    end
+    # Display the volunteers in increments of 100
+    @volunteer_count = (@volunteer_count / 100).floor * 100
+
+    @featured_projects = Rails.cache.fetch('featured_projects', expires_in: 1.day) do
+      Project.where(highlight: true).order('RANDOM()').take 3
+    end
   end
 
   private
