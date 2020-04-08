73263e1823da5d90232ba07d9368fd63eae2e502
Casey Pugh <156097+caseypugh@users.noreply.github.com>
2020-04-08 10:09:20 -0400

    schema timestamp update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_030212) do
+ActiveRecord::Schema.define(version: 2020_04_06_185148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,7 +118,6 @@ ActiveRecord::Schema.define(version: 2020_04_06_030212) do
     t.integer "project_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "note", default: "", null: false
   end
 
   add_foreign_key "taggings", "tags"
