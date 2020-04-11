96cc9acec6dc33cb9d075e9ca120171008aa5da8
Adrian Marin <adrian@adrianthedev.com>
2020-04-11 13:48:32 +0300

    fix: cover photo was not turned to null if deleted

--- app/controllers/projects_images_controller.rb
+++ app/controllers/projects_images_controller.rb
@@ -2,8 +2,8 @@ class ProjectsImagesController < ApplicationController
   def destroy
     @project = Project.find params[:project_id]
     @image = @project.images.find params[:id]
+    @project.update(cover_photo_id: nil) if @project.cover_photo_id == params[:id].to_i
     @image.purge
-    $project.update(cover_photo_id: nil) if @project.cover_photo_id == params[:id]
 
     respond_to do |format|
       format.html { redirect_back fallback_location: edit_project_path(@project), notice: 'Image destroyed.' }
