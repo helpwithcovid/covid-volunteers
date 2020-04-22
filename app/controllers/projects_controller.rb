81e50ac69c814e1896a751cfef8ae3ba17515d0d
Mark Santolucito <mark.santolucito@yale.edu>
2020-04-22 17:15:50 -0400

    change text, fixes #74

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,7 +16,7 @@ class ProjectsController < ApplicationController
     respond_to do |format|
       format.html do
         @projects_header = 'New Haven Residents vs. COVID-19'
-        @projects_subheader = 'This is a New Haven-wide volunteer management website, where New Haven residents can volunteer (in-person or remotely) and local non-profits and government can post volunteer needs. Let us unite and fight the pandemic together!'
+        @projects_subheader = 'This is a New Haven-wide partnership platform, where New Haven residents can volunteer (in-person or remotely) and local non-profits and government can post volunteer needs. Let us unite and fight the pandemic together!'
         @page_title = 'All Projects'
 
         @projects = Kaminari.paginate_array(@projects.select {|project| project.is_visible_to_user?(current_user)})
