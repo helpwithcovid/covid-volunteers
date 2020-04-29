e5e7733b80f6da57210de319d41bcd038e12460d
Shen Sat <shensat@shens-mbp.home>
2020-04-29 12:16:40 +0100

    Add tests for index action

--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -4,77 +4,152 @@ RSpec.describe Users::RegistrationsController, type: :controller do
   set_devise_mapping_to_user
 
   describe 'GET #index' do
-    before { get :index }
-
-    context 'with a volunteer' do
-      it 'assigns filters open variable' do
+    let(:do_request) { get :index }
+    let!(:user) { create(:user, visibility: true) }
+    
+    context 'with no sign-in' do
+      before do
+        do_request
       end
 
       it 'shows/assigns search bar' do
+        expect(assigns(:show_search_bar)).to eq(true)
       end
 
       it 'shows/assigns sorting options' do
+        expect(assigns(:show_sorting_options)).to eq(true)
       end
 
       it 'shows/assigns filters' do
+        expect(assigns(:show_filters)).to eq(true)
       end
 
       it 'is successful' do
+        expect(response).to be_successful
       end
-
+        
       it 'returns all the volunteers' do
+        expect(assigns(:users)).to eq([ user ])
       end
 
       context 'when skills are selected' do
-
+        let(:do_request) { get :index, params: params }
+        let(:params) { { skills: ['Analytics'] } }
+            
         it 'returns no volunteers' do
+          expect(assigns(:users)).to eq([])
         end
+
         context 'when a user has matching skills' do
+          let!(:user) { create(:user, visibility: true, skill_list: ['Analytics']) }
+
           it 'returns the volunteer' do
+            expect(assigns(:users)).to eq([ user ])
           end
         end
-
       end
 
       context 'when a search term is provided' do
+        let(:do_request) { get :index, params: params }
+        let(:params) { { query: 'paris' } }
+
         it 'returns no volunteers' do
+          expect(assigns(:users)).to eq([])
         end
 
         context 'when a user has matching value' do
+          let!(:user) { create(:user, visibility: true, location: 'Paris') }
+          
           it 'returns the volunteer' do
+            expect(assigns(:users)).to eq([ user ])
           end
         end
       end
 
       context 'when volunteer visibility is false' do
-        context 'when user is not admin' do
-          it 'does not return user' do
+        let!(:user) { create(:user, visibility: false) }
+          
+        it 'does not return volunteer' do
+          expect(assigns(:users)).to eq([])
+        end
+      end
+
+      context 'with two volunteers' do
+        let!(:user) { create(:user, visibility: true, created_at: Date.parse('01/01/2020')) }
+        let!(:user_two) { create(:user, visibility: true, created_at: Date.parse('31/01/2020')) }
+
+        context 'if sort latest is selected' do
+          let(:do_request) { get :index, params: params }
+          let(:params) { { sort_by: 'latest' } }
+
+          it 'orders them from latest to earliest' do
+            expect(assigns(:users).first).to eq(user_two)
+            expect(assigns(:users).last).to eq(user)
           end
         end
 
-        context 'when user is admin' do
-          it 'returns user' do
-          end
+        context 'if sort earliest is selected' do
+          let(:do_request) { get :index, params: params }
+          let(:params) { { sort_by: 'earliest' } }
+
+          it 'orders them from earliest to latest' do
+            expect(assigns(:users).first).to eq(user)
+            expect(assigns(:users).last).to eq(user_two)
+          end 
         end
-      end
+      end 
     end
 
-    context 'with more than one volunteer' do
-      it 'orders them' do
+
+    context 'when admin is signed-in' do
+      let (:admin) { create(:user, email: ADMINS[0]) }
+
+      before do
+        sign_in admin
+
+        do_request
       end
+
+      context 'when volunteer visibility is false' do
+        let!(:user) { create(:user, visibility: false) }
+          
+        it 'returns volunteer' do
+          expect(assigns(:users)).to include(user)
+        end
+      end      
     end
 
-    context 'with 26 volunteers' do
-      context 'when first page is accessed' do
-        it 'returns the first 25 volunteers' do
+    context 'with twenty-six volunteers' do
+      before do
+        25.times { create(:user, visibility:true) }
+
+        do_request
+      end
+
+      context 'when first page is requested' do
+        let(:do_request) { get :index, params: params }
+        let(:params) { { page: 1 } }
+
+        it 'returns the first twenty-five volunteers' do
+          last_user = User.last
+
+          expect(assigns(:users).size).to eq(25)
+          expect(assigns(:users)).to_not include(last_user)
         end
       end
 
-      context 'when second page is accessed' do
-        it 'returns the 26th volunteer' do
+      context 'when second page is requested' do
+        let(:do_request) { get :index, params: params }
+        let(:params) { { page: 2 } }
+
+        it 'returns the twenty-sixth volunteer' do
+          last_user = User.last
+
+          expect(assigns(:users).size).to eq(1)
+          expect(assigns(:users)).to include(last_user)
         end
       end
-    end
+    end  
   end
 
   describe 'GET #show' do
