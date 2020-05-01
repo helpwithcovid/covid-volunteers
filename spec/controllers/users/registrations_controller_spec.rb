5b752ff26af4f54fad26e17669ce5558b3cc2652
Shen Sat <shensat@shens-mbp.home>
2020-05-01 10:31:32 +0100

    Refactor index action tests

--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -2,79 +2,100 @@ require 'rails_helper'
 
 RSpec.describe Users::RegistrationsController, type: :controller do
   set_devise_mapping_to_user
+  render_views
 
   describe 'GET #index' do
     let(:do_request) { get :index }
-    let!(:user) { create(:user_visible) }
 
-    shared_examples 'it assigns volunteer' do
-      it 'assigns volunteer' do
-        expect(assigns(:users)).to eq([ user ])
+    shared_examples 'it displays volunteer' do
+      it 'displays volunteer' do
+        expect(response.body).to include(user.name)
       end
     end
 
-    shared_examples 'it does not assign volunteer' do
-      it 'does not assign volunteer' do
-        expect(assigns(:users)).to eq([])
+    shared_examples 'it does not display volunteer' do
+      it 'does not display volunteer' do
+        expect(response.body).to_not include(user.name)
       end
     end
     
-    context 'with no sign-in' do
-      before { do_request }
+    context 'with one volunteer' do
+      let!(:user) { create(:user_visible) }
+      
+      context 'with no sign-in' do
+        before { do_request }
 
-      it 'shows/assigns search bar' do
-        expect(assigns(:show_search_bar)).to eq(true)
-      end
+        it 'shows/assigns search bar' do
+          expect(assigns(:show_search_bar)).to eq(true)
+        end
 
-      it 'shows/assigns sorting options' do
-        expect(assigns(:show_sorting_options)).to eq(true)
-      end
+        it 'shows/assigns sorting options' do
+          expect(assigns(:show_sorting_options)).to eq(true)
+        end
 
-      it 'shows/assigns filters' do
-        expect(assigns(:show_filters)).to eq(true)
-      end
+        it 'shows/assigns filters' do
+          expect(assigns(:show_filters)).to eq(true)
+        end
 
-      it 'is successful' do
-        expect(response).to be_successful
-      end
-        
-      it_behaves_like 'it assigns volunteer'
+        it 'is successful' do
+          expect(response).to be_successful
+        end
+          
+        it_behaves_like 'it displays volunteer'
 
-      context 'when skills are selected' do
-        let(:do_request) { get :index, params: params }
-        let(:params) { { skills: ['Analytics'] } }
+        context 'when skills are selected' do
+          let(:do_request) { get :index, params: params }
+          let(:params) { { skills: ['Analytics'] } }
+              
+          it_behaves_like 'it does not display volunteer'
+
+          context 'when a user has matching skills' do
+            let!(:user) { create(:user_visible, skill_list: ['Analytics']) }
+
+            it_behaves_like 'it displays volunteer'
+          end
+        end
+
+        context 'when a search term is provided' do
+          let(:do_request) { get :index, params: params }
+          let(:params) { { query: 'paris' } }
+
+          it_behaves_like 'it does not display volunteer'
+
+          context 'when a user has matching value' do
+            let!(:user) { create(:user_visible, location: 'Paris') }
             
-        it_behaves_like 'it does not assign volunteer'
-
-        context 'when a user has matching skills' do
-          let!(:user) { create(:user_visible, skill_list: ['Analytics']) }
-
-          it_behaves_like 'it assigns volunteer'
+            it_behaves_like 'it displays volunteer'
+          end
         end
+
+        context 'when volunteer visibility is false' do
+          let!(:user) { create(:user, visibility: false) }
+            
+          it_behaves_like 'it does not display volunteer'
+        end 
       end
 
-      context 'when a search term is provided' do
-        let(:do_request) { get :index, params: params }
-        let(:params) { { query: 'paris' } }
+      context 'when admin is signed-in' do
+        let (:admin) { create(:user, email: ADMINS[0]) }
 
-        it_behaves_like 'it does not assign volunteer'
+        before do
+          sign_in admin
 
-        context 'when a user has matching value' do
-          let!(:user) { create(:user_visible, location: 'Paris') }
-          
-          it_behaves_like 'it assigns volunteer'
+          do_request
         end
+
+        context 'when volunteer visibility is false' do
+          let!(:user) { create(:user, visibility: false) }
+            
+          it_behaves_like 'it displays volunteer'
+        end      
       end
 
-      context 'when volunteer visibility is false' do
-        let!(:user) { create(:user, visibility: false) }
-          
-        it_behaves_like 'it does not assign volunteer'
-      end
+      context 'with a second volunteer' do
+        let!(:user_two) { create(:user_visible, created_at: Date.tomorrow) }
 
-      context 'with two volunteers' do
-        let!(:user) { create(:user_visible, created_at: Date.parse('01/01/2020')) }
-        let!(:user_two) { create(:user_visible, created_at: Date.parse('31/01/2020')) }
+        before { do_request }
 
         context 'if sort latest is selected' do
           let(:do_request) { get :index, params: params }
@@ -95,30 +116,13 @@ RSpec.describe Users::RegistrationsController, type: :controller do
             expect(assigns(:users).last).to eq(user_two)
           end 
         end
-      end 
-    end
-
-    context 'when admin is signed-in' do
-      let (:admin) { create(:user, email: ADMINS[0]) }
-
-      before do
-        sign_in admin
-
-        do_request
       end
-
-      context 'when volunteer visibility is false' do
-        let!(:user) { create(:user, visibility: false) }
-          
-        it 'returns volunteer' do
-          expect(assigns(:users)).to include(user)
-        end
-      end      
     end
 
     context 'with twenty-six volunteers' do
       before do
-        25.times { create(:user, visibility:true) }
+        25.times { create(:user_visible, name: 'Any Name') }
+        create(:user_visible, name: 'Volunteer 26')
 
         do_request
       end
@@ -127,11 +131,10 @@ RSpec.describe Users::RegistrationsController, type: :controller do
         let(:do_request) { get :index, params: params }
         let(:params) { { page: 1 } }
 
-        it 'returns the first twenty-five volunteers' do
-          last_user = User.last
+        it 'only returns the first twenty-five volunteers' do
 
-          expect(assigns(:users).size).to eq(25)
-          expect(assigns(:users)).to_not include(last_user)
+          expect(response.body).to include('Any Name')
+          expect(response.body).to_not include('Volunteer 26')
         end
       end
 
@@ -140,10 +143,9 @@ RSpec.describe Users::RegistrationsController, type: :controller do
         let(:params) { { page: 2 } }
 
         it 'returns the twenty-sixth volunteer' do
-          last_user = User.last
 
-          expect(assigns(:users).size).to eq(1)
-          expect(assigns(:users)).to include(last_user)
+          expect(response.body).to_not include('Any Name')
+          expect(response.body).to include('Volunteer 26')
         end
       end
     end  
