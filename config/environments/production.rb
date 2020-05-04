0328deee0299a5e6491a152fd850358690a7f8c7
Radu Spineanu <radu.spineanu@gmail.com>
2020-05-04 11:50:07 -0700

    uncomment asset from cdn for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV['CDN_URL'] if ENV['CDN_URL'].present?
+  # config.action_controller.asset_host = ENV['CDN_URL'] if ENV['CDN_URL'].present?
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
