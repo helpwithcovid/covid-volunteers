32798ebdfe279e28623cbd4ba7e1bb6de21583cc
Shen Sat <shensat@shens-mbp.home>
2020-05-05 10:27:48 +0100

    Correct spacing and syntax errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,7 +66,7 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Warden::Test::Helpers
   config.include Devise::Test::IntegrationHelpers, type: :request
-  config.extend ControllerMacros, :type => :controller
+  config.extend ControllerMacros, type: :controller
 
   config.include FactoryBot::Syntax::Methods
 end
