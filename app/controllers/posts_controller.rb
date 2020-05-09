edbdd36beda5fabab9da5dee048d0df122198156
Adrian Marin <adrian@adrianthedev.com>
2020-05-09 11:07:12 +0300

    refactor: remove starter code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,6 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :set_bg_white
 
-  # GET /posts
-  # GET /posts.json
   def index
     params[:page] ||= 1
 
@@ -15,12 +13,9 @@ class PostsController < ApplicationController
     @total_count = @posts.total_count
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
   end
 
-  # GET /posts/new
   def new
     unless current_user.is_admin?
       flash[:message] = 'You have to be an admin to add posts.'
@@ -31,7 +26,6 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
     unless current_user.is_admin?
       flash[:message] = 'You have to be an admin to add posts.'
@@ -40,14 +34,12 @@ class PostsController < ApplicationController
     end
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
     @post = Post.new(post_params)
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Success story was successfully created.' }
+        format.html { redirect_to @post, notice: 'Story was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -56,12 +48,10 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Success story was successfully updated.' }
+        format.html { redirect_to @post, notice: 'Story was successfully updated.' }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
@@ -70,23 +60,19 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Success story was successfully destroyed.' }
+      format.html { redirect_to posts_url, notice: 'Story was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def post_params
       params.fetch(:post, {}).permit(:title, :body, :links, :image, :project_ids)
     end
