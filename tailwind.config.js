276fa34b5b29968fb34b6ba17a374c9aad5ee6e3
Adrian Marin <adrian@adrianthedev.com>
2020-07-10 13:03:22 +0300

    refactor: use tailwind ui colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 const plugin = require('tailwindcss/plugin')
-const { colors } = require('tailwindcss/defaultTheme')
+const colors = require('@tailwindcss/ui/colors')
 
 module.exports = {
   theme: {
@@ -11,7 +11,7 @@ module.exports = {
           ...colors.indigo,
         },
         secondary: {
-          ...colors.purple,
+          ...colors.gray,
         },
       },
       maxHeight: {
