5db43e68c7d31820b42ba00e3a26bcf140de4e1b
Adrian Marin <adrian@adrianthedev.com>
2020-07-13 18:09:45 +0300

    refactor: update the theme config file

--- theme/config.rb
+++
@@ -1,5 +0,0 @@
-# This file is loaded when Rails boots during config/initializers.
-# Set up default variables based on our `settings.yml`
-
-Settings.add_source!("#{Rails.root}/theme/settings.yml")
-Settings.reload!
