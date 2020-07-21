6860ab70eb8c92ae0a0877723514d45870ec61ae
Jamie Wilkinson <jamie@jamiedubs.com>
2020-07-21 16:21:25 -0400

    Prime Heroku post-deploy

--- deploy-hwbe.sh
+++ deploy-hwbe.sh
@@ -2,3 +2,4 @@
 # Creates a 'helpwithblackequity' remote if you don't have one
 heroku git:remote --app helpwithblackequity -r helpwithblackequity
 git push helpwithblackequity helpwithblackequity:master
+curl -s -i https://helpwithblackequity.herokuapp.com | head -n1
